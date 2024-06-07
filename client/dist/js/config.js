a09c934dd8d41eb40def0c7ff0ac602b69bfb496
EC2 Default User <ec2-user@ip-172-31-68-32.ec2.internal>
2024-06-07 20:11:06 +0000

    pongo jhone en par cosas

--- client/dist/js/config.js
+++ client/dist/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
   "api": {
-    "invokeUrl": "https://ubz5pglg68.execute-api.us-east-1.amazonaws.com/dev/ride"
+    "invokeUrl": "https://jrz6iety4a.execute-api.us-east-1.amazonaws.com/dev/ride"
   },
   "cognito": {
-    "userPoolId": "us-east-1_Wm6LflME2",
-    "userPoolClientId": "1pn8qu2ph5iqv3sq6cnkc3gkec",
+    "userPoolId": "us-east-1_MLBLayfFZ",
+    "userPoolClientId": "k01553hut7c91oqqjdllo07qb",
     "region": "us-east-1",
     "disabled": false
   }
